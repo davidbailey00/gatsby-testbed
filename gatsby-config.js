65df29a834cdfea96c7349e6f113fe27d2b5326f
David Bailey <david.j.b@vivaldi.net>
2018-08-13 14:41:32 +0100

    Move posts back to src (better practice)

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,8 +6,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `posts`,
-        path: `${__dirname}/static/posts/`,
+        name: `src`,
+        path: `${__dirname}/src/`,
       },
     },
     `gatsby-transformer-remark`,
