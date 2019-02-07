f8570a57698d0f8bcf227ce9968f4d861cbe7293
David Bailey <david.j.b@vivaldi.net>
2019-02-07 15:58:06 +0000

    prettierrc + multilingual 404 example

---
+++ src/pages/en/404.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../../components/layout"
+import Head from "../../components/head"
+
+export default () => (
+  <Layout>
+    <Head title="404" />
+    <h1>Page Not Found</h1>
+    <p>English-specific 404 page</p>
+  </Layout>
+)
---
+++ src/pages/en/600.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../../components/layout"
+import Head from "../../components/head"
+
+export default () => (
+  <Layout>
+    <Head title="600" />
+    <h1>Normal page for testing</h1>
+    <p>Hello, world</p>
+  </Layout>
+)
